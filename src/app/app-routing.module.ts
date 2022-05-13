7ecba93bf1d5198f218f062dea3b7c3630531a61
garyyuhsuan <42322051+garyyuhsuan@users.noreply.github.com>
2022-05-13 18:09:42 +0800

    link fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,9 +9,9 @@ import { TodoComponent }   from './todo/todo.component';
 
 const routes: Routes = [
   { path: '', redirectTo:'pokemon', pathMatch:'full'},
-  { path: 'LearnAngular/search', component: SearchComponent },
-  { path: 'LearnAngular/pokemon', component: PokemonComponent },
-  { path: 'LearnAngular/todo', component: TodoComponent }
+  { path: 'search', component: SearchComponent },
+  { path: 'pokemon', component: PokemonComponent },
+  { path: 'todo', component: TodoComponent }
 ];
 
 @NgModule({
